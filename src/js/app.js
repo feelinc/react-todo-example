1fa5b33eef7747ba41d9b0a41a7604711f78fb43
Sulaeman <me@sulaeman.com>
2015-05-11 19:59:52 +0700

    remove some description

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,3 @@
-/*
- * This file is part of the Keys Digital Delivery
- *
- * Author: Sulaeman <me@sulaeman.com>.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
- 
 var React = require('react');
 
 var TodoApp = require('./components/TodoApp.react');
